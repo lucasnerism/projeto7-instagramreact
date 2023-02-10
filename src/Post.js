0d8ad86edaceedb4ef9fa4a28a14afc521bcfabf
Lucas Neris <lucasnerism@gmail.com>
2023-02-10 05:48:03 -0300

    requisitos obrigatórios finalizados

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,14 @@
-let likes = 10;
+import React from "react";
 
 export default function Post(props) {
-  likes = Math.floor(Math.random() * 100);
+  const [likes, setLikes] = React.useState(Math.floor(Math.random() * 100));
+  const [salvo, setSalvo] = React.useState("bookmark-outline");
+  const [classe, setClasse] = React.useState("");
+  const [coracao, setCoracao] = React.useState("heart-outline");
+
   if (props.video != undefined) {
     return (
-      <div class="post">
+      <div class="post" data-test="post">
         <div class="topo">
           <div class="usuario">
             <img src={props.imagemusuario} alt={props.nomeusuario} />
@@ -16,7 +20,13 @@ export default function Post(props) {
         </div>
 
         <div class="conteudo">
-          <video autoPlay muted>
+          <video data-test="post-imageDouble" onClick={() => {
+            if (coracao === "heart-outline") {
+              setCoracao("heart");
+              setClasse("vermelho");
+              setLikes(likes + 1);
+            }
+          }} autoPlay muted>
             <source src={props.video} type="video/mp4" />
             <source src={props.videoogv} type="video/ogv" />
             "Seu navegador não suporta vídeos."
@@ -26,26 +36,39 @@ export default function Post(props) {
         <div class="fundo">
           <div class="acoes">
             <div>
-              <ion-icon name="heart-outline"></ion-icon>
+              <ion-icon data-test="like-post" class={classe} name={coracao} onClick={() => {
+                if (coracao === "heart-outline") {
+                  setCoracao("heart");
+                  setClasse("vermelho");
+                  setLikes(likes + 1);
+                } else {
+                  setCoracao("heart-outline");
+                  setClasse("");
+                  setLikes(likes - 1);
+                }
+              }}></ion-icon>
               <ion-icon name="chatbubble-outline"></ion-icon>
               <ion-icon name="paper-plane-outline"></ion-icon>
             </div>
             <div>
-              <ion-icon name="bookmark-outline"></ion-icon>
+              <ion-icon name={salvo} onClick={() => {
+                if (salvo === "bookmark-outline") setSalvo("bookmark");
+                else setSalvo("bookmark-outline");
+              }}></ion-icon>
             </div>
           </div>
 
           <div class="curtidas">
             <img src="assets/img/kayds.png" alt="kayds" />
             <div class="texto">
-              Curtido por <strong>kayds</strong> e <strong>outras {likes} pessoas</strong>
+              Curtido por <strong>kayds</strong> e <strong>outras</strong> <strong data-test="likes-number">{likes}</strong> <strong>pessoas</strong>
             </div>
           </div>
         </div>
       </div>
     );
   } else return (
-    <div class="post">
+    <div class="post" data-test="post">
       <div class="topo">
         <div class="usuario">
           <img src={props.imagemusuario} alt={props.nomeusuario} />
@@ -57,18 +80,37 @@ export default function Post(props) {
       </div>
 
       <div class="conteudo">
-        <img src={props.imagem} alt={props.altimagem} />
+        <img data-test="post-image" onClick={() => {
+          if (coracao === "heart-outline") {
+            setCoracao("heart");
+            setClasse("vermelho");
+            setLikes(likes + 1);
+          }
+        }} src={props.imagem} alt={props.altimagem} />
       </div>
 
       <div class="fundo">
         <div class="acoes">
           <div>
-            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon data-test="like-post" class={classe} name={coracao} onClick={() => {
+              if (coracao === "heart-outline") {
+                setCoracao("heart");
+                setClasse("vermelho");
+                setLikes(likes + 1);
+              } else {
+                setCoracao("heart-outline");
+                setClasse("");
+                setLikes(likes - 1);
+              }
+            }}></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
           <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
+            <ion-icon data-test="save-post" name={salvo} onClick={() => {
+              if (salvo === "bookmark-outline") setSalvo("bookmark");
+              else setSalvo("bookmark-outline");
+            }}></ion-icon>
           </div>
         </div>
 
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,11 +1,21 @@
+import React from "react";
+
 export default function Usuario() {
+  const [user, setUser] = React.useState("lucasnerism");
+  const [img, setImg] = React.useState("./assets/img/kayds.png");
+
+
   return (
     <div class="usuario">
-      <img src="./assets/img/kayds.png" alt="imagem de perfil" />
+      <img data-test="profile-image" src={img} alt="imagem de perfil" onClick={() => {
+        const novaimagem = prompt("Qual a nova imagem que você deseja utilizar?"); if (novaimagem != "" && novaimagem != undefined && novaimagem != null) setImg(novaimagem);
+      }} />
       <div class="texto">
         <span>
-          <strong>lucasnerism</strong>
-          <ion-icon name="pencil"></ion-icon>
+          <strong data-test="name">{user}</strong>
+          <ion-icon data-test="edit-name" onClick={() => {
+            const novoNome = prompt("Qual nome você deseja utilizar?"); if (novoNome != "" && novoNome != undefined && novoNome != null) setUser(novoNome);
+          }} name="pencil"></ion-icon>
         </span>
       </div>
     </div>
